fdecd46a0e8fa31fc46efb70c3ffd8e99144a890
redassser <redryan890@gmail.com>
2025-01-21 16:12:07 -0500

    initial assignments, weird hardware issues

--- RoboLegs.ino
+++ RoboLegs.ino
@@ -2,25 +2,53 @@
 
 Servo HipL;
 Servo HipR;
-Servo LegL;
-Servo LegR;
-Servo KneeL;
-Servo KneeR;
+Servo LegLL;
+Servo LegLR;
+Servo LegRL;
+Servo LegRR;
+Servo KneeLL;
+Servo KneeLR;
+Servo KneeRL;
+Servo KneeRR;
 Servo AnkleL;
 Servo AnkleR;
 
 void setup() {
-  HipL.attach(0);
-  HipR.attach(0);
-  LegL.attach(0);
-  LegR.attach(0);
-  KneeL.attach(0);
-  KneeR.attach(0);
-  AnkleL.attach(0);
-  AnkleR.attach(0);
+  HipL.attach(30);
+  HipR.attach(31);
+
+  LegLL.attach(32);
+  LegLR.attach(33);
+  LegRL.attach(34);
+  LegRR.attach(35);
+
+
+  KneeLL.attach(36);
+  KneeLR.attach(37);
+  KneeRL.attach(38);
+  KneeRR.attach(39);
+  
+  AnkleL.attach(40);
+  AnkleR.attach(41);
+  
+
+  HipL.write(110);
+  HipR.write(80);
+
+  LegLL.write(150);
+  LegLR.write(30);
+  LegRL.write(150);
+  LegRR.write(30);
+  
+  KneeLL.write(150);
+  KneeLR.write(30);
+  KneeRL.write(150);
+  KneeRR.write(30);
+  
+  AnkleL.write(100);
+  AnkleR.write(80);
+  
 }
 
 void loop() {
-  AnkleL.write(90);
-  AnkleR.write(90);
 }
